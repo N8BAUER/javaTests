6e12d54ff114b5c6b6008fb5f39044f98ff11071
Nate <n8bdoesemailvia@gmail.com>
2017-10-23 15:50:12 -0600

    sampling Java primitatives, objects, classes, while, fors, and basics println printf

---
+++ application.java
@@ -0,0 +1,34 @@
+public class application {
+
+  public static void main(String[] args){
+    System.out.println("Hello World");
+
+    //Primitive data types
+    int myNumber = 9999;
+
+    short myShort = 847;
+
+    long myLong = 89898;
+
+    double myDouble = 7.34334343;
+
+    float myFloat = 324.3f;
+
+    char myChar  = 'y';
+
+    boolean myBoolean = false;
+
+    byte myByte = 23;
+
+
+    System.out.println(myNumber);
+    System.out.println(myShort);
+    System.out.println(myDouble);
+    System.out.println(myFloat);
+    System.out.println(myChar);
+    System.out.println(myBoolean);
+    System.out.println(myByte);
+
+  }
+
+}
---
+++ tutorial5.java
@@ -0,0 +1,7 @@
+public class tutorial5 {
+  public static void main(String[] args) {
+    for(int i=0; i < 5; i = i +1){
+      System.out.printf("The value of is: %d\n", i);
+    }
+  }
+}
